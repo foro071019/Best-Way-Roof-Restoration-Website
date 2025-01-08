1ed33902285ad52039b83ae5f9e1d2a1a986722b
foro071019 <rejayforo@gmail.com>
2025-01-09 07:03:16 +0800

    Update header.php

--- header.php
+++ header.php
@@ -5,13 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Best Way Roof Restoration</title>
 
-    <link rel="stylesheet" href="bootstrap-5.3.3-dist/css/bootstrap.min.css">
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'><link rel="stylesheet" href="./style.css">
-    <link rel="stylesheet" href="assets/css/global.css">
-    <link rel="stylesheet" href="assets/css/home_page.css">
-    <link rel="stylesheet" href="assets/css/home_page_header.css">
-    <link rel="stylesheet" href="assets/css/home_page_footer.css">
-
     <?php
         wp_head();
     ?>
